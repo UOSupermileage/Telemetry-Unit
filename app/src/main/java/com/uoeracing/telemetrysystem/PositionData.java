30cda017fcb41db03badeb70294dd40c8293472d
bijx <bsamiee@hotmail.com>
2018-01-07 01:05:37 -0500

    Added positional data point object for run data collection.

---
+++ app/src/main/java/com/uoeracing/telemetrysystem/PositionData.java
@@ -0,0 +1,84 @@
+package com.uoeracing.telemetrysystem;
+
+/**
+ * Positional Data Point Object
+ *
+ * This class represents a single point on the map. There will be many of these per trip, each representing a data point on the map consisting of several data variables collected at the instant.
+ */
+
+public class PositionData {
+
+    // Class variables to store instantaneous vehicular data.
+    private int currentTime, currentLap, height;
+    private double longitude, latitude, speed;
+    private boolean isInclined;
+
+    // Constructor defines all relevant data to be logged in object.
+    public PositionData(int currentTime, int currentLap, int height, double longitude, double latitude, double speed, boolean isInclined) {
+        this.currentTime = currentTime;
+        this.currentLap = currentLap;
+        this.height = height;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.speed = speed;
+        this.isInclined = isInclined;
+    }
+
+
+    // Getters and Setters
+    public int getTime() {
+        return currentTime;
+    }
+
+    public void setTime(int currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    public int getLap() {
+        return currentLap;
+    }
+
+    public void setLap(int currentLap) {
+        this.currentLap = currentLap;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public boolean isInclined() {
+        return isInclined;
+    }
+
+    public void setInclined(boolean inclined) {
+        isInclined = inclined;
+    }
+}
