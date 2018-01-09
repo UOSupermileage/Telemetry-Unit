32d727a1d4ae910cad34eab327f7357fa2eefc91
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2018-01-09 17:32:00 -0500

    Added maps to MainLayout

---
+++ app/src/main/java/com/uoeracing/telemetrysystem/MapActivity.java
@@ -0,0 +1,49 @@
+package com.uoeracing.telemetrysystem;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LatLngBounds trackBounds;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sonoma Raceway Location and move the camera
+        LatLng raceway = new LatLng(38.1609, 122.4547);
+
+        mMap.addMarker(new MarkerOptions().position(raceway).title("Sonoma Raceway"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(raceway));
+    }
+}
