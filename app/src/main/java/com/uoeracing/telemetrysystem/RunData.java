1a4929a36f5531a736cac4211b94ea3fd4115498
bijx <bsamiee@hotmail.com>
2018-01-13 12:59:38 -0500

    Added run class

---
+++ app/src/main/java/com/uoeracing/telemetrysystem/RunData.java
@@ -0,0 +1,18 @@
+package com.uoeracing.telemetrysystem;
+
+import java.util.ArrayList;
+
+/**
+ * Run Data Object
+ *
+ * The run data class will store all positional data points for future viewing on the map. Each data point in the
+ * list will be added to a marker on the map with data from that instance.
+ */
+
+public class RunData {
+
+    ArrayList dataPositions = new ArrayList();
+
+
+
+}
