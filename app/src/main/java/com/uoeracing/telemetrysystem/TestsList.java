f692fd8be19658a501cbfa4b402c1523ee1d93b1
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2018-01-16 22:27:31 -0500

    changed id's

--- app/src/main/java/com/uoeracing/telemetrysystem/TestsList.java
+++ app/src/main/java/com/uoeracing/telemetrysystem/TestsList.java
@@ -25,8 +25,8 @@ public class TestsList extends ArrayAdapter<RunData> {
         LayoutInflater inflater = context.getLayoutInflater();
         View listViewItem = inflater.inflate(R.layout.layout_tests, null, true);
 
-        TextView textTotalTime = (TextView) listViewItem.findViewById(R.id.totalTime);
-        TextView textTotalLaps = (TextView) listViewItem.findViewById(R.id.totalLaps);
+        TextView textTotalTime = (TextView) listViewItem.findViewById(R.id.nameOfRun);
+        TextView textTotalLaps = (TextView) listViewItem.findViewById(R.id.startDate);
 
         RunData run = tests.get(position);
         textTotalTime.setText(String.valueOf(run.getRunName()));
